1f9c19eb0952366ed9d137a441bdca3efbadfae9
0jayer <100233258+0jayer@users.noreply.github.com>
2023-07-18 06:42:44 +0600

    Update main.cpp

--- Graph/main.cpp
+++ Graph/main.cpp
@@ -5,14 +5,40 @@ using namespace std;
 
 int main()
 {
-    GraphType<string>g(8);
+    GraphType<char>g(8);
     for(int i=0;i<8;i++)
     {
         char c;
         cin>>c;
-
+        g.AddVertex(c);
     }
+    g.AddEdge('A','B',1);
+    g.AddEdge('B','A',1);
+    g.AddEdge('A','C',1);
+    g.AddEdge('A','D',1);
+    g.AddEdge('D','A',1);
+    g.AddEdge('D','E',1);
+    g.AddEdge('D','G',1);
+    g.AddEdge('G','F',1);
+    g.AddEdge('F','H',1);
+    g.AddEdge('H','E',1);
 
+    int res = g.OutDegree('D');
+    cout<<res<<endl;
+    if(g.FoundEdge('A','D'))
+        cout<<"There is an edge."<<endl;
+    else
+        cout<<"There is no edge.\n";
+
+    if(g.FoundEdge('B','D'))
+        cout<<"There is an edge."<<endl;
+    else
+        cout<<"There is no edge.\n";
+    g.DepthFirstSearch('B','E');
+    g.DepthFirstSearch('E','B');
+    g.BreadthFirstSearch('B','E');
+    g.BreadthFirstSearch('E','B');
+    g.BreadthFirstSearchlen('B','E');
 
 }
 
